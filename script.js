d6e95418bbe9edd562debb6b86066c11e6ce3b4d
Roman <tsoyroman2003@gmail.com>
2023-08-11 12:18:14 +0600

    Practice lesson 1

---
+++ script.js
@@ -0,0 +1,21 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const movie1 = prompt('Один из последних просмотренных фильмов?'),
+    rate1 = prompt('На сколько его оцените?');
+
+const movie2 = prompt('Второй из последних просмотренных фильмов?'),
+    rate2 = prompt('На сколько его оцените?');
+
+
+personalMovieDB.movies[movie1] = rate1;
+personalMovieDB.movies[movie2] = rate2;
+
+console.log(personalMovieDB);
